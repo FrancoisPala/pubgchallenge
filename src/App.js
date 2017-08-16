e16d2e16646e051081c651c4a102324a555dbe41
François Palà <fpala@kilix.fr>
2017-08-16 17:53:30 +0200

    added stuff

--- src/App.js
+++ src/App.js
@@ -10,11 +10,12 @@ class App extends Component {
 
     const first = First[Math.floor(Math.random() * (First.length - 0))];
     const second = first.table[Math.floor(Math.random() * (first.table.length - 0) + 0)];
-    const sentence = `${first.phrase}${second.title} ${second.hint} Worth ${second.points} useless internet points`;
+    const sentence = `${first.phrase}${second.title} ${second.hint} Worth ${second.points} useless internet points.`;
 
     return (
       <div className="sentence">
-        {sentence}
+        <p>{sentence}</p>
+        <p className="bottomnote">Don't forget, if you win the challenge you can brag about it to your one other friend who plays.</p>
       </div>
     );
   }
--- src/tables/locations.js
+++ src/tables/locations.js
@@ -88,7 +88,122 @@ const Locations = [
 		'title': 'School.',
 		'hint': 'South West of Rozhok, the big square building and the ones east of it.',
 		'points': 30
-	}
+	},
+	{
+		'title': 'Villa.',
+		'hint': 'South West of Yasnaya Polyana.',
+		'points': 60,
+	},
+	{
+		'title': 'Lipovka.',
+		'hint': 'East of the island.',
+		'points': 50,
+	},
+	{
+		'title': 'Prison (Crater).',
+		'hint': 'West of Lipovka. AKA "Where everybody goes and we always die there".',
+		'points': 30,
+	},
+	{
+		'title': 'Shelter (Bunkers).',
+		'hint': 'West of Lipovka, West of Prison (Crater).',
+		'points': 30,
+	},
+	{
+		'title': 'Pochinki.',
+		'hint': 'Where we all go. You, me, my grandma etc. Also known as "Prone City".',
+		'points': 40,
+	},
+	{
+		'title': 'Gatka.',
+		'hint': 'South of Georgopol, West of Pochinki.',
+		'points': 50,
+	},
+	{
+		'title': 'Gatka Trenches.',
+		'hint': 'South-East of Gatka.',
+		'points': 70,
+	},
+	{
+		'title': 'Pochinki South Hills.',
+		'hint': 'Pretty much self explanatory.',
+		'points': 60,
+	},
+	{
+		'title': 'Woodcutter\'s Camp.',
+		'hint': 'North-West of Mylta Power.',
+		'points': 70,
+	},
+	{
+		'title': 'Mylta Power.',
+		'hint': 'East of the Island, under Lipovka.',
+		'points': 30,
+	},
+	{
+		'title': 'Mylta.',
+		'hint': 'Just above the Eastern bridge of the Military Island.',
+		'points': 50,
+	},
+	{
+		'title': 'Mylta West.',
+		'hint': 'At this point if you don\'t know your West from East, there\'s nothing I can do for you.',
+		'points': 60,
+	},
+	{
+		'title': 'Quarry.',
+		'hint': 'South-West of the map, above Primorsk.',
+		'points': 50,
+	},
+	{
+		'title': 'Pier Town.',
+		'hint': 'West of the western bridge of the Military Island.',
+		'points': 60,
+	},
+	{
+		'title': 'Military Base.',
+		'hint': 'South of the map, on the second Island. Good luck.',
+		'points': 40,
+	},
+	{
+		'title': 'Novorepnoye Radio.',
+		'hint': 'Between Military Base and Novorepnoye.',
+		'points': 60,
+	},
+	{
+		'title': 'Novorepnoye.',
+		'hint': 'South-East of the map, or East on the Military Base Island (Sosnovka).',
+		'points': 30,
+	},
+	// {
+	// 	'title': '',
+	// 	'hint': '',
+	// 	'points': ,
+	// },
+	// {
+	// 	'title': '',
+	// 	'hint': '',
+	// 	'points': ,
+	// },
+	// {
+	// 	'title': '',
+	// 	'hint': '',
+	// 	'points': ,
+	// },
+	// {
+	// 	'title': '',
+	// 	'hint': '',
+	// 	'points': ,
+	// },
+	// {
+	// 	'title': '',
+	// 	'hint': '',
+	// 	'points': ,
+	// },
+	// {
+	// 	'title': '',
+	// 	'hint': '',
+	// 	'points': ,
+	// },
 ];
 
 export default Locations;
