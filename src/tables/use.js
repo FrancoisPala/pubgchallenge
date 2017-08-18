ed4e3314a577641fcf7825bcb00ced9f10179535
François Palà <fpala@kilix.fr>
2017-08-18 16:44:19 +0200

    more weapon possibilities

--- src/tables/use.js
+++ src/tables/use.js
@@ -1,17 +1,27 @@
 const use = [
 	{
-		title: 'an Assault Rifle.',
-		hint: 'Can be an AK, M16A4, M416, SCAR-L.',
+		title: 'Assault Rifles.',
+		hint: 'Can be an AK, M16A4, M416, SCAR-L, Groza.',
+		points: 30,
+	},
+	{
+		title: 'Sniper Rifles.',
+		hint: 'Can be a VSS, SKS, MK14 EBR, KAR98K, M24, AWM.',
 		points: 30,
 	},
 	{
 		title: 'SMGs.',
-		hint: 'Can be an UMP9, Vector, Mini-Uzi, Groza.',
+		hint: 'Can be an UMP9, Vector, Micro Uzi, Tommy Gun.',
 		points: 50,
 	},
 	{
+		title: 'Shotguns.',
+		hint: 'Can be a S686, S1897, S12K.',
+		points: 70,
+	},
+	{
 		title: 'Guns (sorry).',
-		hint: 'Symptoms include G18C, .',
+		hint: 'Symptoms include P18C, P92, P1911, R1895.',
 		points: 90,
 	},
 ];
