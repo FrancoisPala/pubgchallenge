a69b2a4ff10d7ee8893a912f8041d54d19925c9a
François Palà <fpala@kilix.fr>
2017-08-21 10:19:56 +0200

    changed readme

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import First from './tables/first';
+import R from 'ramda';
 
 class App extends Component {
     constructor(props) {
@@ -12,20 +13,37 @@ class App extends Component {
                 'hardcore': this.filterHardcore,
             },
             sentence: '',
+            used: [],
         };
     }
     componentDidMount() { this.generateSentence() }
     filterEasy(list) { return list.filter(element => element.points < 40 ? element : null);}
     filterHard(list) { return list.filter(element => (element.points >= 40 && element.points < 70) ? element : null);}
     filterHardcore(list) { return list.filter(element => element.points >= 70 ? element : null);}
+
+    changedUsed(used) {
+        const usedVar = this.state.used.slice();
+        usedVar.push(used);
+        this.setState({used: usedVar});
+    }
     changeSentence(sentence) {
         this.setState({sentence: sentence});
     }
+    getFirst() {
+        let result;
+        while(!R.contains(result, this.state.used) || this.state.used.length < First.length) {
+            result = First[Math.floor(Math.random() * First.length)];
+            this.changedUsed(result);
+        }
+        return result;
+    }
     generateSentence(choice = 'random') {
         const first = First[Math.floor(Math.random() * First.length)];
+        // const first = this.getFirst(); boucle inf
         const table = choice !== 'random' ? this.state.fctPtr[choice](first.table) : first.table;
         const second = table[Math.floor(Math.random() * table.length)];
         const sentence = `${first.phrase}${second.title} ${second.hint} Worth ${second.points} useless internet points.`;
+        this.changedUsed(first);
         this.changeSentence(sentence);
     }
     render() {
@@ -35,6 +53,9 @@ class App extends Component {
         // ajouter de la difficulte. Basse: 0-39 points et moins, Moyen: de 40 a 69 points, Haute: 70+;
 
         // console.log(sentence);
+
+        // FAIRE UN CODE PEN QUI PREND UNE PHRASE DE LETTRES, LES REORGANISE, LES SHUFFLE, PUIS LA REMPLACE AVEC UNE NOUVELLE PHRASE GO TRANSITION
+
         return (
             <div className="sentence">
                 <div id='table'>
